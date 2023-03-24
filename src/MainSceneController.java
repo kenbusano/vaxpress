8d5fac5c1e073cc5ce3f18fb0e9670bb4cd1a74c
kenbusano2003 <kenbusano2003@gmail.com>
2023-03-25 00:00:08 +0800

    whitespaces remove

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -4,15 +4,12 @@ import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 public class MainSceneController {
-
     @FXML
     private TextField message;
-
     @FXML
     void btnOK(ActionEvent event) {
         Stage mainWindow = (Stage) message.getScene().getWindow();
         String title = message.getText();
         mainWindow.setTitle(title);
     }
-
 }
