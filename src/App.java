0397d6fbfbf4692badaf638cb4249b7048868fa6
kenbusano2003 <kenbusano2003@gmail.com>
2023-03-25 08:11:21 +0800

    remove whitespaces

--- src/App.java
+++ src/App.java
@@ -6,17 +6,14 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
  
 public class App extends Application {
-
-    private Parent createContent() {
+    public Parent createContent() {
         return new StackPane(new Text("Hello World"));
     }
-
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(createContent(), 300, 300));
         stage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
