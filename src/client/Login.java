1837067f8601e6fef601d1b388e75654e7ab36a0
kenbusano <kenbusano2003@gmail.com>
2023-04-04 21:33:40 +0800

    3434343$

--- src/client/Login.java
+++ src/client/Login.java
@@ -1,5 +1,26 @@
 package client;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Login {
-    
+    @FXML
+    private Button proceedBtn;
+
+    @FXML
+    private void proceedBtn(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/Login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) proceedBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
