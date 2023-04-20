e5bc62b71e574011bfc4d55fa875d766b2218991
kenbusano2003 <kenbusano2003@gmail.com>
2023-04-21 00:40:19 +0800

    working

--- src/client/RegisterNotice.java
+++ src/client/RegisterNotice.java
@@ -7,16 +7,33 @@
 
 package client;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class RegisterNotice {
     @FXML
     private Button noticeBtn;
 
     @FXML
-    private void handleNotice(ActionEvent event) {
-        /* maya na me magprovide */
+    private void startRegister(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/RegisterPt1.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+            Stage stage = (Stage) noticeBtn.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("FXML document not found");
+            e.printStackTrace();
+        }
     }
 }
