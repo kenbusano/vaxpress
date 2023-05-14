0e8d60ee337d2f6c1030df972fec3e0a07e1f9cf
kenbusano <kenbusano2003@gmail.com>
2023-05-14 16:22:58 +0800

    error muna

--- src/client/Login.java
+++ src/client/Login.java
@@ -8,18 +8,13 @@
 package client;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-import server.TunnelAdmin;
 
 public class Login {
     @FXML
@@ -27,12 +22,6 @@ public class Login {
 
     @FXML
     private Button registerBtn;
-
-    @FXML
-    private TextField userField;
-
-    @FXML
-    private PasswordField passField;
 
     @FXML
     private void proceedBtn(ActionEvent event) throws IOException {
@@ -53,39 +42,6 @@ public class Login {
         Stage stage = (Stage) registerBtn.getScene().getWindow();
         stage.setScene(scene);
         stage.show();
-    }
-    @FXML
-    private void Admin() {
-        String username = userField.getText();
-        String password = passField.getText();
-    
-        try {
-            if (TunnelAdmin.AdminVal(username, password)) {
-                System.out.println("ayan gumagana na");
-            } else {
-                System.out.println("gagoooo mali");
-            }
-        } catch (SQLException e) {
-            TunnelAdmin.printSQLException(e);
-        }
-    }
-    @FXML
-    private void loginNow(ActionEvent event) {
-        Admin();
-    }
-    
-    public void initialize() {
-        userField.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                passField.requestFocus();
-            }
-        });
-
-        passField.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                Admin();
-            }
-        });
     }
 }
 
