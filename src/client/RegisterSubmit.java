5277a70942b0c474ee47cd5f67fc5686fc4b2875
kenbusano <kenbusano2003@gmail.com>
2023-05-16 04:40:25 +0800

    gggG

--- src/client/RegisterSubmit.java
+++ src/client/RegisterSubmit.java
@@ -5,6 +5,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.stage.Stage;
 
 public class RegisterSubmit {
     @FXML
@@ -19,6 +25,17 @@ public class RegisterSubmit {
     private DatePicker birthday;
     @FXML
     private TextField birthPlace;
+    @FXML
+    private Button proceedBtn;
 
-    
+    @FXML
+    private void proceedNext(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/RegisterPt2.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+        Stage stage = (Stage) proceedBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
