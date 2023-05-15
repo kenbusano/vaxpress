1a0f1a0c5ccd58cde440b14a231d27a4cee55b9e
kenbusano <kenbusano2003@gmail.com>
2023-05-16 00:41:22 +0800

    wait lang

--- src/admin/Dashboard.java
+++ src/admin/Dashboard.java
@@ -19,7 +19,33 @@ import javafx.stage.Stage;
 public class Dashboard {
     @FXML
     private Button logOut;
-    
+
+    @FXML
+    private Button vaccReq;
+
+    @FXML
+    private Button vaccHome;
+
+    @FXML
+    private void adminHome (ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/AdminPanel.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+        Stage stage = (Stage) logOut.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void vaccine (ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/AdminVacc.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+        Stage stage = (Stage) logOut.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
     private void signOut (ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/client/resources/Login.fxml"));
